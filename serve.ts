537fbc29784b1e8858bfd41d1d461b414b250af9
Clement Denis <le.mikmac@gmail.com>
2022-07-21 16:54:15 +0100

    fix payment button

--- serve.ts
+++
@@ -1,5 +0,0 @@
-import { serve } from "https://deno.land/std@0.143.0/http/server.ts"
-import { serveDir } from "https://deno.land/std@0.143.0/http/file_server.ts"
-
-const opts = { fsRoot: Deno.cwd() }
-serve((req) => serveDir(req, opts))
